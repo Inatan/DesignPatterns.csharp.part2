3e451f0d3c90ad17d882dab503bb9c1bcc6063c8
Inatan <inatan_hertzog@hotmail.com>
2020-11-22 21:16:52 -0300

    Factory

--- DesignPatterns.csharp.part2/Program.cs
+++ DesignPatterns.csharp.part2/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace DesignPatterns.csharp.part2
+﻿using DesignPatterns.csharp.part2.Cap1;
+using System.Data;
+
+namespace DesignPatterns.csharp.part2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "select * from tabela";
+
         }
     }
 }
