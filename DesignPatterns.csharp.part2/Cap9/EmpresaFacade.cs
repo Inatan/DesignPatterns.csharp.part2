16ebf8186c111e17e98a3381fc57c3680d9b7ffa
Inatan <inatan_hertzog@hotmail.com>
2020-11-24 21:07:58 -0300

    Facede and singleton - Don't use that

---
+++ DesignPatterns.csharp.part2/Cap9/EmpresaFacade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.csharp.part2.Cap9
+{
+    public class EmpresaFacade
+    {
+        // exemplo de legado
+        //public Cliente BuscaCliente(string cpf)
+        //{
+        //    return new ClieanteDAO().BuscaPorCpf(cpf);
+        //}
+
+        //public Fatura CriaFatura(Cliente cliente, double valor)
+        //{
+        //    return new Fatura(cliente, valor);
+        //}
+
+        //public Cobranca GeraCobranca(Tipo tipo, Fatura fatura)
+        //{
+        //    GeraCobranca cobranca = new Cobranca(tipo, fatura);
+        //    cobranca.Emite();
+        //}
+
+        //public ContatoCliente FazContato(Cliente cliente, Cobranca cobranca)
+        //{
+        //    ContatoCliente contat new ContatoCliente(cliente, cobranca);
+            
+        //}
+    }
+}
--- DesignPatterns.csharp.part2/Program.cs
+++ DesignPatterns.csharp.part2/Program.cs
@@ -5,10 +5,14 @@ using DesignPatterns.csharp.part2.Cap4;
 using DesignPatterns.csharp.part2.Cap5;
 using DesignPatterns.csharp.part2.Cap6;
 using DesignPatterns.csharp.part2.Cap7;
+using DesignPatterns.csharp.part2.Cap8;
+using DesignPatterns.csharp.part2.Cap9;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq.Expressions;
+using System.Xml.Serialization;
 
 namespace DesignPatterns.csharp.part2
 {
@@ -82,17 +86,45 @@ namespace DesignPatterns.csharp.part2
             //mensagem.Enviador = enviador;
             //mensagem.Envia();
 
-            // Cap 7
+            // Cap 7 - Command
 
-            FilaDeTrabalho fila = new FilaDeTrabalho();
-            Pedido pedido1 = new Pedido("Mauricio", 100.0);
-            Pedido pedido2 = new Pedido("Marcelo", 200.0);
+            //FilaDeTrabalho fila = new FilaDeTrabalho();
+            //Pedido pedido1 = new Pedido("Mauricio", 100.0);
+            //Pedido pedido2 = new Pedido("Marcelo", 200.0);
 
-            fila.Adiciona(new PagaPedido(pedido1));
-            fila.Adiciona(new PagaPedido(pedido2));
+            //fila.Adiciona(new PagaPedido(pedido1));
+            //fila.Adiciona(new PagaPedido(pedido2));
 
-            fila.Adiciona(new FinalizaPedido(pedido1));
-            fila.Processa();
+            //fila.Adiciona(new FinalizaPedido(pedido1));
+            //fila.Processa();
+
+            // Cap 8 - Adapter
+            //Cliente cliente = new Cliente();
+            //cliente.Nome = "Victor";
+            //cliente.Endereco = "Rua Vergueiro";
+            //cliente.DataDeNascimento = DateTime.Now;
+
+            //string xml = new GeradorDeXml().GeraXml(cliente);
+
+            //Console.WriteLine(xml);
+
+            // Cap 9 - Façade - Singleton
+            //String cpf = "1234";
+            //EmpresaFacade facade = new EmpresaFacadeSingleton().Instancia;
+            //Cliente cliente = facade.BuscaCliente();
+
+            //fatura = facade.CriaFatura(cliente, 5000);
+            //facade.GeraCobranca(tipo.Boleto, fatura)
+                
+            //Cliente cliente
+            //     = new ClientDao().BuscaPorCpf(cpf);
+            //Fatura fatura = new Fatura(cliente, 5000);
+            //Cobranca cobranca = new Cobranca(Tipo.Boleto, fatura);
+            //cobranca.Emite();
+
+            //ContatoCliente contat = new ContatoCliente(cliente, cobranca);
+            //contato.Dispara();
+
 
             Console.ReadKey();
         }
